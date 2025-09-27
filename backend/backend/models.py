0cdde1ef98cdfc54fa2a10f37dea0e561b893e84
Yorles25 <108990295@ina.cr>
2025-09-26 22:12:52 -0400

    Update models.py

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,10 +1,11 @@
 # backend/models.py
-from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy import Column, Integer, String, Date
 from .db import Base
 
 class HistoricalData(Base):
     __tablename__ = "historical_data"
-
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(String, index=True)   # en el futuro lo cambiamos a Date
-    value = Column(Float)
+    fecha = Column(String, nullable=False)  # o Date si quieres
+    md = Column(Integer, nullable=False)
+    td = Column(Integer, nullable=False)
+    nc = Column(Integer, nullable=False)
